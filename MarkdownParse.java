b305196295000a238498851d3abd48fc886c5343
kaijia2022 <kaz006@ucsd.edu>
2022-05-19 19:35:42 -0700

    code changes and screenshots for lab report 4

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -42,7 +42,24 @@ public class MarkdownParse {
                 closeParen = markdown.indexOf(")", closeParen+1);
             }
             if (openParen + 1 <= closeParen){
-                toReturn.add(markdown.substring(openParen + 1, closeParen));
+                //toReturn.add(markdown.substring(openParen + 1, closeParen));
+                
+                if (beforNewLine(markdown, closeParen)){
+                    int innercloseParen = getOuterParen(markdown, closeParen);
+                    toReturn.add(trimSpaces(markdown,openParen,innercloseParen));
+                }
+                else{
+                    toReturn.add(trimSpaces(markdown,openParen,closeParen));
+                }
+                
+                if (isNestedLink(markdown, openBracket,closeBracket,closeParen)){
+                    openParen = markdown.indexOf("(", closeParen);
+                    closeParen = markdown.indexOf(")", openParen);
+                    closeParen = getOuterParen(markdown, closeParen);
+                    //toReturn.add(markdown.substring(openParen + 1, closeParen));
+                    toReturn.add(trimSpaces(markdown,openParen,closeParen));
+                }
+
             }
             else{
                 break;
@@ -53,10 +70,46 @@ public class MarkdownParse {
         return toReturn;
    
     }
+    public static boolean isNestedLink(String md, int openBra, int closeBra,int closePar){
+        openBra = md.indexOf("[",openBra+1);
+        int newLine = md.indexOf("\n",closeBra);
+        System.out.println(newLine);
+        return openBra != -1 && openBra < closeBra && md.substring(closeBra,newLine).contains("]")
+        && !isEscapeCharacter(md, openBra);
+    }
+
+    public static int getOuterParen(String md, int closeParen){
+        
+        int newLine = md.indexOf("\n",closeParen);
+        /*System.out.println(closeParen+1);
+        System.out.println(newLine);*/
+        int outerCloseParen = closeParen;
+        System.out.println(md.substring(outerCloseParen+1, newLine).contains(")"));
+        while (md.substring(outerCloseParen+1, newLine).contains(")")){
+            outerCloseParen = md.indexOf(")", outerCloseParen+1);
+            
+            //System.out.println(closeParen);
+        }
+        return outerCloseParen;
+    }
 
     private static boolean isEscapeCharacter(String md, int index){
         if (index - 1 > -1 && md.charAt(index-1) == '\\'){
-            System.out.println("true");
+            //System.out.println("true");
+            return true;
+        }
+        return false;
+    }
+
+    public static String trimSpaces(String md, int openParen, int closeParen){
+        String toTrim = md.substring(openParen+1, closeParen);
+        return toTrim.trim();
+    }   
+    public static boolean beforNewLine(String md, int closeParen){
+        int newLine = md.indexOf("\n",closeParen);
+        System.out.println("newline: "+ newLine);
+        System.out.println("closeParen: " + closeParen);
+        if (!md.substring(closeParen,newLine).contains("]")){
             return true;
         }
         return false;
@@ -64,8 +117,7 @@ public class MarkdownParse {
 
 
 
-
-    public static void main(String[] args) throws IOException {
+  public static void main(String[] args) throws IOException {
         Path fileName = Path.of(args[0]);
         String content = Files.readString(fileName);
         ArrayList<String> links = getLinks(content);
--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -7,12 +7,12 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 
 public class MarkdownParseTest {
-    @Test
+    //@Test
     public void addition(){
         assertEquals(5,3+2);
     }
 
-    @Test
+    //@Test
     public void test_getLink() throws IOException{
         String[] args = new String[1];
         args[0] = "test-file8.md";
@@ -32,7 +32,7 @@ public class MarkdownParseTest {
         
     }
 
-    @Test
+    //@Test
     public void failingTest() throws IOException{
         String[] args = new String[1];
         args[0] = "test5-file.md";
@@ -43,6 +43,70 @@ public class MarkdownParseTest {
         String[] expected = new String[1];
         expected[0] = "hello.com";
         //expected[1] = "some-thing.html";
+
+        String[] actual = new String[links.size()];
+        for (int i = 0; i < actual.length; i++){
+            actual[i] = links.get(i);
+        }
+        assertArrayEquals(expected, actual);
+    }
+
+
+    @Test
+    public void test_md1() throws IOException{
+        String[] args = new String[1];
+        args[0] = "report-test-1.md";
+        Path fileName = Path.of(args[0]);
+        String content = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(content);
+
+        String[] expected = new String[4];
+        expected[0] = "url.com";
+        expected[1] = "`google.com";
+        expected[2] = "google.com";
+        expected[3] = "ucsd.edu";
+
+        String[] actual = new String[links.size()];
+        for (int i = 0; i < actual.length; i++){
+            actual[i] = links.get(i);
+        }
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void test_md2() throws IOException{
+        String[] args = new String[1];
+        args[0] = "report-test-2.md";
+        Path fileName = Path.of(args[0]);
+        String content = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(content);
+
+        String[] expected = new String[4];
+        expected[0] = "a.com";
+        expected[1] = "b.com";
+        expected[2] = "a.com(())";
+        expected[3] = "example.com";
+
+        String[] actual = new String[links.size()];
+        for (int i = 0; i < actual.length; i++){
+            actual[i] = links.get(i);
+        }
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void test_md3() throws IOException{
+        String[] args = new String[1];
+        args[0] = "report-test-3.md";
+        Path fileName = Path.of(args[0]);
+        String content = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(content);
+
+        String[] expected = new String[4];
+        expected[0] = "https://www.twitter.com";
+        expected[1] = "https://sites.google.com/eng.ucsd.edu/cse-15l-spring-2022/scheduleb.com";
+        expected[2] = "github.com";
+        expected[3] = "https://cse.ucsd.edu/";
 
         String[] actual = new String[links.size()];
         for (int i = 0; i < actual.length; i++){
